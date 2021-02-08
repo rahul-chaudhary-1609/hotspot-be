438c74f0aec9c7d2906033b30ede17087cda4a82
Rahul Chaudhary <rahulchaudhary99r@gmail.com>
2021-02-08 17:59:01 +0530

    fav restaurant model added

---
+++ models/fav_restaurant.model.js
@@ -0,0 +1,49 @@
+'use strict';
+const {
+    Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+    class FavRestaurant extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+            FavRestaurant.belongsTo(models.Restaurant);
+            FavRestaurant.belongsTo(models.Customer);
+        }
+    }
+    FavRestaurant.init({
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+
+        restaurant_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+
+        customer_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+
+        is_deleted: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+            defaultValue: false
+        },
+
+    }, {
+        sequelize,
+        underscored: true,
+        tableName: 'fav_restaurants',
+        modelName: 'FavRestaurant',
+    });
+    return FavRestaurant;
+};
