10be0a4b27619883f61a7e8e190dbbada3b68bc2
Rahul Chaudhary <rahulchaudhary99r@gmail.com>
2021-02-09 17:09:30 +0530

    dish_cotegory and restaurant_dish model created

---
+++ models/dish_cotegory.model.js
@@ -0,0 +1,41 @@
+'use strict';
+const {
+    Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+    class DishCategory extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+        }
+    }
+    DishCategory.init({
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        name: {
+            type: DataTypes.STRING(45),
+            allowNull: false,
+        },
+
+        is_deleted: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+            defaultValue: false
+        },
+
+    }, {
+        sequelize,
+        underscored: true,
+        tableName: 'dish_categories',
+        modelName: 'DishCategory',
+    });
+    return DishCategory;
+};
---
+++ models/restaurant_dish.model.js
@@ -0,0 +1,65 @@
+'use strict';
+const {
+    Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+    class RestaurantDish extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+            RestaurantDish.belongsTo(models.Restaurant);
+            RestaurantDish.belongsTo(models.DishCategory);
+        }
+    }
+    RestaurantDish.init({
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        price: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        description: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+
+        restaurant_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+
+        dish_category_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        image_url: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+
+        is_deleted: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+            defaultValue: false
+        },
+
+    }, {
+        sequelize,
+        underscored: true,
+        tableName: 'restaurant_dishes',
+        modelName: 'RestaurantDish',
+    });
+    return RestaurantDish;
+};
