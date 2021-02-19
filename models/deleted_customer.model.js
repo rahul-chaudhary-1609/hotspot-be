622f5c2075f83a26f680ec29ad9681816fb226e0
Rahul Chaudhary <rahulchaudhary99r@gmail.com>
2021-02-19 15:43:02 +0530

    delete a restaurant with reason API created

---
+++ models/deleted_customer.model.js
@@ -0,0 +1,48 @@
+'use strict';
+const {
+    Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+    class DeletedCustomer extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+            DeletedCustomer.belongsTo(models.Customer)
+            DeletedCustomer.belongsTo(models.Admin)
+        }
+    }
+    DeletedCustomer.init({
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+
+        customer_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+
+        admin_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+
+        reason: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+
+    }, {
+        sequelize,
+        underscored: true,
+        tableName: 'deleted_customers',
+        modelName: 'DeletedCustomer',
+    });
+    return DeletedCustomer;
+};
