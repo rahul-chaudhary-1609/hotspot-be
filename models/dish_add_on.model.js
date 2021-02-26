e13627cede0687d8f771e1e7a9d243e17137fdda
Rahul Chaudhary <rahulchaudhary99r@gmail.com>
2021-02-26 18:08:01 +0530

    dish add-on model created

---
+++ models/dish_add_on.model.js
@@ -0,0 +1,56 @@
+'use strict';
+const {
+    Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+    class DishAddOn extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+            DishAddOn.belongsTo(models.RestaurantDish);
+        }
+    }
+    DishAddOn.init({
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        price: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+
+        image_url: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        restaurant_dish_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+
+        is_deleted: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+            defaultValue: false
+        },
+
+    }, {
+        sequelize,
+        underscored: true,
+        tableName: 'dish_add_ons',
+        modelName: 'DishAddOn',
+    });
+    return DishAddOn;
+};
