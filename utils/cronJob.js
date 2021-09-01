d8d6776eb477278a374cd76becd94b1c08761be4
Rahul Chaudhary <rahul.chaudhary@algoworks.com>
2021-09-01 13:22:56 +0530

    testing

--- utils/cronJob.js
+++ utils/cronJob.js
@@ -257,7 +257,7 @@ module.exports.scheduleRestaurantOrdersEmailJob = async()=> {
 
                     //let deliveryDatetime = new Date(`${utilityFunctions.getOnlyDate(new Date())} ${nextDeliveryTime}+330`);
                     // let deliveryDatetime = new Date(`2021-06-28 ${nextDeliveryTime}+00`);
-                    let deliveryDatetime = new Date(`2021-09-01 13:00:00+00`);
+                    let deliveryDatetime = new Date(`2021-09-01 13:30:00+00`);
                     let cutOffTime = new Date(`${utilityFunctions.getOnlyDate(new Date())} ${getCutOffTime(nextDeliveryTime || "00:00:00")}`);
 
                     let orders = await utilityFunctions.convertPromiseToObject(
