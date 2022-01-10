9e6d775ab8090d08c837b36125f29b6f423514c6
Rahul Chaudhary <rahul.chaudhary@algoworks.com>
2022-01-10 22:01:51 +0530

    refund email added

--- services/admin/refund.service.js
+++ services/admin/refund.service.js
@@ -3,6 +3,288 @@ const { Op } = require("sequelize");
 const constants = require("../../constants");
 const utility = require('../../utils/utilityFunctions');
 const stripe = require('stripe')(constants.STRIPE.stripe_secret_key);
+const sendMail = require('../../utils/mail');
+const moment =require("moment");
+
+const sendRefundEmail= async (params) => {
+
+    console.log("send Order Payment Email", params)
+
+    let bodyHTML = `<div style="background-color:#e6e8e6;border-radius: 5px;padding: 15px;">
+    <div style="text-align: center;">
+        <img src="https://hotspot-customer-profile-picture1.s3.amazonaws.com/admin/other/download%20%288%29_1622468052927.png" alt="">
+    </div>
+
+    <div style="background-color:#fff; border-radius: 25px;padding: 20px;margin: 15px;">
+        <div style="margin-top: 15px;">
+            <div >
+                <Strong>Order ID:</strong> #${params.order.order_id}<br/>
+                <Strong>Refund Type:</strong> ${params.refundType==constants.REFUND_TYPE.company_credit?"Hotspot Credit":"Card Refund"}
+            </div>
+            
+
+            <div style="margin-top: 30px;">
+                <div>
+                    -For: ${params.order.order_details.customer.name} -
+                </div>
+    `;
+
+    
+    bodyHTML += `<div style="margin-top: 10px;">
+    <table style="width: 100%;">`
+
+
+
+    params.order.order_details.ordered_items.forEach((ordered_item)=>{
+        bodyHTML +=`<tr style="vertical-align: top;">
+                            <td style="text-align: left;">
+                                <div>
+                                    ${ordered_item.itemCount}x
+                                </div>
+                            </td>
+                            <td>
+                                <div>
+                                    ${ordered_item.itemName}
+                                    ($${(parseFloat(ordered_item.price)*ordered_item.itemCount).toFixed(2)})`
+                                    if(ordered_item.is_refunded){
+                                        bodyHTML +=`<span 
+                                                        style="font-size:10px;color:white;
+                                                            margin:0px 5px 0px 5px;
+                                                            background-color:red;
+                                                            border-radius:10px;
+                                                            padding:1px 5px 1px 5px;"
+                                                    >${ordered_item.refund_count}x refunded</span>`
+                                        
+                                        bodyHTML +=`<span 
+                                                        style="color:red;margin-right:10px;">
+                                                        (- $${(parseFloat(ordered_item.refund_amount)).toFixed(2)})
+                                                    </span>`
+                                    }
+                                    
+
+        ordered_item.itemAddOn.forEach((addOn)=>{
+            bodyHTML +=`<li style="font-size: 13px;">${addOn.name}
+                ($${(parseFloat(addOn.price)*ordered_item.itemCount).toFixed(2)})`
+                if(addOn.is_refunded){
+                    bodyHTML +=`<span 
+                                    style="font-size:10px;color:white;
+                                        margin:0px 5px 0px 5px;
+                                        background-color:red;
+                                        border-radius:10px;
+                                        padding:1px 5px 1px 5px;"
+                                >${addOn.refund_count}x refunded</span>`
+                    
+                    bodyHTML +=`<span 
+                                    style="color:red;margin-right:10px;">
+                                    (- $${(parseFloat(addOn.refund_amount)).toFixed(2)})
+                                </span>`
+                }
+            bodyHTML +=`</li>`
+        })
+
+        if(ordered_item.preference && ordered_item.preference.trim()!==""){
+            bodyHTML +=`<div>
+                <i>Preference: </i>
+                    <span style="font-size: 13px;">
+                        ${ordered_item.preference}
+                    </span>
+            </div>`
+        }
+
+        bodyHTML += `</div>
+            </td>
+            <td style="text-align: right;">
+                <div>
+                    $${(parseFloat(ordered_item.itemPrice)).toFixed(2)}
+                </div>
+            </td>
+        </tr>`
+        
+    })
+        
+    bodyHTML +=`</table>
+    </div>`
+
+    bodyHTML+=`<div style="margin-top: 10px;">
+        <table style="width: 100%;">
+            <tr style="text-align: left; vertical-align: top; ">
+                <td style="text-align: left; border-top:2px solid #e6e8e6;">
+                    <div>
+                        Subtotal
+                    </div>
+                </td>
+                <td style="text-align: right; border-top:2px solid #e6e8e6;">
+                    <div>`
+                        if(params.order.order_details.amount_details.refundSubtotal){
+                            bodyHTML+=`<span style="color:red;margin-left:10px;">(- $${(parseFloat(params.order.order_details.amount_details.refundSubtotal)).toFixed(2)})</span>`
+                        }
+                    bodyHTML+=`$${params.order.order_details.amount_details.subtotal.toFixed(2)}
+                    </div>
+                </td>
+            </tr>
+            <tr style="text-align: left; vertical-align: top; ">
+                <td style="text-align: left;">
+                    <div>
+                        Regulatory Response Fee
+                    </div>
+                </td>
+                <td style="text-align: right;">
+                    <div>
+                        Free
+                    </div>
+                </td>
+            </tr>
+            <tr style="text-align: left; vertical-align: top; ">
+                <td style="text-align: left;">
+                    <div>
+                        Delivery Fee
+                    </div>
+                </td>
+                <td style="text-align: right;">
+                    <div>
+                        Free
+                    </div>
+                </td>
+            </tr>
+            <tr style="text-align: left; vertical-align: top; ">
+                <td style="text-align: left;">
+                    <div>
+                        Service Fee
+                    </div>
+                </td>
+                <td style="text-align: right;">
+                    <div>
+                        Free
+                    </div>
+                </td>
+            </tr>
+            <tr style="text-align: left; vertical-align: top; ">
+                <td style="text-align: left;">
+                    <div>
+                        Processing Fee (${params.order.order_details.amount_details.processing_fee_variable_percentage}%${params.order.order_details.amount_details.processing_fee_fixed_amount?` + ¢${params.order.order_details.amount_details.processing_fee_fixed_amount}`:``})
+                    </div>
+                </td>
+                <td style="text-align: right;">
+                    <div>
+                        $${params.order.order_details.amount_details.processing_fee.toFixed(2)}
+                    </div>
+                </td>
+            </tr>
+            <tr style="text-align: left; vertical-align: top; ">
+                <td style="text-align: left;">
+                    <div>
+                        Taxes (${params.order.order_details.amount_details.taxes_variable_percentage}%${params.order.order_details.amount_details.taxes_fixed_amount?` + ¢${params.order.order_details.amount_details.taxes_fixed_amount}`:``})
+                    </div>
+                </td>
+                <td style="text-align: right;">
+                    <div>`
+                        if(params.order.order_details.amount_details.refundSalesFee){
+                            bodyHTML+=`<span style="color:red;margin-left:10px;">(- $${(parseFloat(params.order.order_details.amount_details.refundSalesFee)).toFixed(2)})</span>`
+                        }
+                        bodyHTML+=`$${params.order.order_details.amount_details.taxes.toFixed(2)}
+                    </div>
+                </td>
+            </tr>
+            <tr style="text-align: left; vertical-align: top; ">
+                <td style="text-align: left;">
+                    <div>
+                        Credits Applied
+                    </div>
+                </td>
+                <td style="text-align: right;">
+                    <div>
+                        -$${params.order.order_details.amount_details.credits_applied.toFixed(2)}
+                    </div>
+                </td>
+            </tr>
+            <tr style="text-align: left; vertical-align: top; ">
+                <td style="text-align: left;">
+                    <div>
+                        Tip
+                    </div>
+                </td>
+                <td style="text-align: right;">
+                    <div>
+                        $${params.order.tip_amount || "0.00"}
+                    </div>
+                </td>
+            </tr>
+        </table>
+    </div>`
+
+    bodyHTML+=`<div style="margin-top: 10px;">
+        <table style="width: 100%;">
+            <tr style="text-align: left; vertical-align: top; ">
+                <td style="text-align: left; border-top:2px solid #e6e8e6;">
+                    <div>
+                        <strong>Total Charged </strong> 
+                    </div>
+                </td>
+                <td style="text-align: right; border-top:2px solid #e6e8e6;">
+                   <div>
+                       <strong>$${params.order.order_details.amount_details.grandTotal.toFixed(2)}</strong>
+                    </div>
+                </td>
+            </tr>
+        </table>                        
+    </div>`
+
+    bodyHTML+=`<div style="margin-top: 10px;">
+        <table style="width: 100%;">
+            <tr style="text-align: left; vertical-align: top; ">
+                <td style="text-align: left; border-top:2px solid #e6e8e6;">
+                    <div>
+                        <strong>Total Cost </strong> 
+                    </div>
+                </td>
+                <td style="text-align: right; border-top:2px solid #e6e8e6;">
+                   <div>
+                       <strong>$${params.order.order_details.amount_details.totalCost.toFixed(2)}</strong>
+                    </div>
+                </td>
+            </tr>
+        </table>                        
+    </div>`
+
+    bodyHTML+=`<div style="margin-top: 10px;">
+        <table style="width: 100%;">
+            <tr style="text-align: left; vertical-align: top; color:red; ">
+                <td style="text-align: left; border-top:2px solid #e6e8e6;">
+                    <div>
+                        <strong>Total Refund </strong> 
+                    </div>
+                </td>
+                <td style="text-align: right; border-top:2px solid #e6e8e6;">
+                   <div>
+                       <strong>$${params.order.order_details.amount_details.refundTotal.toFixed(2)}</strong>
+                    </div>
+                </td>
+            </tr>
+        </table>                        
+    </div>`
+
+    bodyHTML+=`</div>                
+            </div>
+        </div>
+    </div>`
+
+
+        
+    let mailOptions = {
+        from: `Hotspot <${process.env.SG_EMAIL_ID}>`,
+        to: params.order.order_details.customer.email,
+        subject:  `Refund Receipt #${params.order.order_id}`,
+        html: bodyHTML,
+    };
+
+    console.log(mailOptions)    
+    
+    
+    await sendMail.send(mailOptions);
+    
+    return true;
+}
+
 
 
 module.exports = {
@@ -98,7 +380,7 @@ module.exports = {
                 })
             )
 
-            await models.Order.update(
+            let order=await models.Order.update(
                 {
                     order_details:{
                         ...orderPayment.order_details,
@@ -110,7 +392,9 @@ module.exports = {
                 {
                     where:{
                         order_id:orderPayment.order_id,
-                    }
+                    },
+                    returning:true,
+                    raw:true,
                 }
             )
 
@@ -147,6 +431,13 @@ module.exports = {
             let newRefund=await utility.convertPromiseToObject(
                 await models.Refund.create(refundObj)
             )
+
+
+            await sendRefundEmail({
+                order:order[1][0],
+                orderPayment,
+                refundType:params.type
+            })
 
             return {newRefund};
         }
