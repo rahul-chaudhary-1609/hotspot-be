b1f7feb66d2833d536adfcf10f68ffcbfae9f5c0
Rahul Chaudhary <rahul.chaudhary@algoworks.com>
2022-01-16 21:00:56 +0530

    bugfix

--- apiSchema/adminSchema.js
+++ apiSchema/adminSchema.js
@@ -5,7 +5,7 @@ const constants = require("../constants");
 module.exports = {
     login : Joi.object({
         email: Joi.string()
-            .regex(/^(?:^[0-9]{4,15}|[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4})$/i)
+            // .regex(/^(?:^[0-9]{4,15}|[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4})$/i)
             .required()
             .messages(
             {
